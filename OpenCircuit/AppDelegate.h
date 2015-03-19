de76bd4de6fff1364e5cc04ab9f90cb010bc263b
august-of-wind <ameyer5@luc.edu>
2015-03-19 17:44:15 -0500

    Initial Commit

---
+++ OpenCircuit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenCircuit
+//
+//  Created by August Meyer on 3/19/15.
+//  Copyright (c) 2015 LuTec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenCircuit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCircuit
+//
+//  Created by August Meyer on 3/19/15.
+//  Copyright (c) 2015 LuTec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
