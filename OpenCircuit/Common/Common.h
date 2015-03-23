3d5f09e241301f8761898c2d3ce8c80775e34b0a
John O'Sullivan <josullivan1@luc.edu>
2015-03-23 00:13:31 -0500

    Add Files

---
+++ OpenCircuit/Common/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  OpenCircuit
+//
+//  Created by John O'Sullivan on 3/23/15.
+//  Copyright (c) 2015 LuTec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
+@end
---
+++ OpenCircuit/Common/Common.m
@@ -0,0 +1,13 @@
+//
+//  Common.m
+//  OpenCircuit
+//
+//  Created by John O'Sullivan on 3/23/15.
+//  Copyright (c) 2015 LuTec. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+@end
